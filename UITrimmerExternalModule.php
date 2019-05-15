5d04bce79abadaacbeaaa83251101a7674efaf41
G. Rezniczek <g.rezniczek@gmail.com>
2019-05-15 19:09:00 +0200

    Add inline for MyProjects removal.

--- UITrimmerExternalModule.php
+++ UITrimmerExternalModule.php
@@ -141,7 +141,23 @@ class UITrimmerExternalModule extends TranslatableExternalModule {
                 }
             })",
         "remove_myprojects_link" => 
-            "",
+            "$(function() {
+                const eltoremove = $('div.menubox a[href*=\"index.php?action=myprojects\"]')
+                eltoremove.each(function() {
+                    let el = $(this)
+                    let elbefore = el.prev('i')
+                    if (elbefore.length == 1) {
+                        let elafter = el.next('span')
+                        this.previousSibling.nodeValue = ''
+                        elbefore.remove()
+                        elafter.remove()
+                        el.remove()
+                    }
+                    else {
+                        el.removeAttr('href')
+                    }
+                })
+            })",
     );
 
     private function includeScriptlet($name) {
